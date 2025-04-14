df9fa6a834449d3ca977fa39d8bf661bcd8e4741
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-04-14 14:52:51 -0300

    Estudo sobre funções na aula 17

---
+++ Estudos 1/aula17/index.js
@@ -0,0 +1,58 @@
+function soma(n1, n2) {
+    const resultado = n1 + n2;
+    return resultado; // ao encontrar o return nada mais será execultado
+}
+
+/*
+function saudacao(nome /parâmetro/) {
+    return (`Bom dia! ${nome}`); // fazer a função retornar um valor
+}
+saudacao('Jorge');
+saudacao('Maria');
+saudacao('Luiz');
+
+// a função não retorna valor nenhum
+const variavel = saudacao('Jorge');
+console.log(variavel);
+
+// fazer a função retornar um valor utilizando "return"
+// ao encontrar o return nada mais será execultado
+function saudacao(nome) {
+    return console.log(`Bom dia! ${nome}`);
+}
+
+function soma(n1, n2) {
+    const resultado = n1 + n2;
+    console.log('Olá mundo!')
+    return resultado; // ao encontrar o return nada mais será execultado
+}
+
+const resultado = soma(2, 2);
+console.log(resultado);
+
+// utilizando uma função
+function soma(n1, n2) {
+    const resultado = n1 + n2;
+    return resultado;
+}
+
+console.log(soma(2, 2));
+console.log(soma(3, 1));
+console.log(soma(5, 10));
+
+// função anônima
+const raiz = function(n) {
+    return n ** 0.5;
+}; // precisa de ponto e virgula
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+
+// arrow function - simplificação, substituindo function por =>, retirando parenteses, pontos e virgulas e chaves
+const raiz = n => n ** 0.5
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+*/
