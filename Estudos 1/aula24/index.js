00e16196f5e8ad479db3b7fdfea50adaebf9de30
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-04-15 15:08:49 -0300

    aula 24 sobre if e else

---
+++ Estudos 1/aula24/index.js
@@ -0,0 +1,21 @@
+/*
+const hora = 50;
+
+if(hora < 12) {
+    console.log('Bom dia!');
+} else if(hora < 18) {
+    console.log('Boa tarde!');
+} else if(hora < 24) {
+    console.log('Boa noite!');
+} else {
+    console.log('Olá');
+}
+*/
+
+const tenhoDinheiro = true;
+
+if(tenhoDinheiro === true) {
+    console.log('Vou sair de casa!');
+} else {
+    console.log('Não vou sair de casa!')
+}
