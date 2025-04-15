7d41d572885c21dbd722bcfa309834fe7a379230
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-04-15 13:51:58 -0300

    Aula 22 onde foi visto sobre operadores lógicos

---
+++ Estudos 1/aula22/index.js
@@ -0,0 +1,32 @@
+/*
+Operadores Lógicos 
+&& -> AND -> E - Todas as expressões precisam ser verdadeiras para retornar true
+|| -> OR -> OU - Apenas uma expressão precisa ser verdadeira para retornar true
+! -> NOT -> NÃO
+*/
+
+/* && - AND
+const dinheiro = true;
+const sol = true;
+const sair = dinheiro && sol;
+console.log(sair);
+*/
+
+/* || - OR
+const dinheiro = false;
+const sol = true;
+const sair = dinheiro || sol;
+console.log(sair);
+*/
+
+/*
+// form que usuário digitou
+const usuario = 'Jorge';
+const senha = '123456';
+
+const logar = usuario === 'Jorge' && senha === '123456';
+console.log(logar);
+*/
+
+
+console.log(!false);
