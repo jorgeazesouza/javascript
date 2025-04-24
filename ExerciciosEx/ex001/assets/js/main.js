fff05a25856bd8b484bc1d3ce4c039d9ea8a5a38
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-04-24 13:12:33 -0300

    exercícios para treinar

---
+++ ExerciciosEx/ex001/assets/js/main.js
@@ -0,0 +1,4 @@
+// Crie um programa que escreva 'Olá, Mundo!' na tela.
+
+const text = document.querySelector('.container p');
+text.innerHTML = 'Olá Mundo!';
---
+++ ExerciciosEx/ex002/assets/js/main.js
@@ -0,0 +1,6 @@
+// Crie um script que leio o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+const nome = document.querySelector('#nome');
+const idade = document.querySelector('#idade');
+const peso = document.querySelector('#peso');
+
+console.log(nome, idade, peso);
