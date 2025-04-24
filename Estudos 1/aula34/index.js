989d51e32434fb4506ead50dc1229940a0cf1810
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-04-24 14:53:14 -0300

    Estrutura de repetição for

---
+++ Estudos 1/aula34/index.js
@@ -0,0 +1,41 @@
+/*
+console.log('Linha 0');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+*/
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Melancia', 'Melão', 'Kiwi'];
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i} -`,frutas[i]);
+}
+
+/*
+/*
+console.log('Linha 0');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5');
+
+for(let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+// variável inicial; condição; incrementar a variável
+// i - index
+for(let i = 0; i <= 10; i++) {
+    const par = i % 2 === 0 ? 'par' : 'ímpar';
+    console.log(i, par);
+}
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Melancia', 'Melão', 'Kiwi'];
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i} -`,frutas[i]);
+}
+*/
