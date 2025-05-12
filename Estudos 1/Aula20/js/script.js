bfa932469908aae505798beb6394bd2bcbd6f7b6
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-05-12 17:23:53 -0300

    exercício

--- Estudos 1/Aula20/js/script.js
+++ Estudos 1/Aula20/js/script.js
@@ -1,38 +1,28 @@
-// criando uma função para separar do código exterior e não conflitar com outras variáveis
-function meuEscopo() {
+function formulario() {
     const form = document.querySelector('.form');
     const resultado = document.querySelector('.resultado');
 
-    // Array
     const pessoas = [];
-
+    
     function recebeEventoForm(evento) {
-        // Função para evitar que o formulário seja enviado
         evento.preventDefault();
 
-        // Dados dos formulários
         const nome = form.querySelector('.nome');
         const sobrenome = form.querySelector('.sobrenome');
         const peso = form.querySelector('.peso');
         const altura = form.querySelector('.altura');
 
-        // Pegando Array adicionando com .push o objeto com os valores dos formulários
-        pessoas.push({
+        pessoas.push = ({
             nome: nome.value,
             sobrenome: sobrenome.value,
             peso: peso.value,
             altura: altura.value
         });
 
-        // fazer aparecer no console
         console.log(pessoas);
 
-        // adicionar no html
-        resultado.innerHTML += `<p>${nome.value}, ${sobrenome.value}, ${peso.value}, ${altura.value}.</p>`
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
     }
-
-    // Adiciona um observador para meu evento
     form.addEventListener('submit', recebeEventoForm);
 }
-// Fazer nossa função aparecer
-meuEscopo();
+formulario();
--- Estudos 1/aula19/index.js
+++ Estudos 1/aula19/index.js
@@ -1,11 +1,57 @@
 /* 
-Primitivos (imutávis) - string, number, boolean, undefined, null (bigint, symbol) - valores copiados
+Tipos de dados Primitivos (imutávis) - string, number, boolean, undefined, null (bigint, symbol) - valores copiados
 
-Referência (mutável) - array, object, function - passado por referencia
-*/
+Tipos de dados por Referência (mutável) - array, object, function - passado por referencia
 
+// Não estamos mudando o valor primitivo "let" estamos mudando o valor dele de 'Jorge' -> 'Azevedo'
+let nome = 'Jorge';
+nome = 'Azevedo';
+console.log(nome);
+
+// Não podemos mudar o valor de um tipo primitivo
+//          01234
+let nome = 'Jorge';
+nome[0] = 'G'; // string é um valor primitivo, não podemos fazer isso ela é imutável
+console.log(nome[0], nome);
+
+// Valores com tipos primitivos - valores imutáveis
+let a = 'A';
+let b = a; // Copiando o valor de A para variável b
+console.log(a, b); // A A
+
+a = 'Outra coisa';
+console.log(a, b); // Outra coisa A
+
+// Valores passados por referência - valores mutáveis
 let a = [1, 2, 3];
-let b = [...a]; // copia o valor de a sem ser por referencia
+let b = a; // b e a apontam para o mesmo valor na memória
 
-a.pop() = 4
+console.log(a, b); // [ 1, 2, 3 ] [ 1, 2, 3 ]
+
+a.push(4);
+console.log(a, b); // [ 1, 2, 3, 4 ] [ 1, 2, 3, 4 ]
+
+b.pop();
+console.log(b, a); // [ 1, 2, 3 ] [ 1, 2, 3 ]
+
+let c = b; // aponta para o mesmo local na memória de a e b
+console.log(c); // [ 1, 2, 3 ]
+
+// copiar valores sem apoontar para mesmo local da memória mas utilizando valores por referencia
+let a = [1, 2, 3];
+let b = [...a]; // valor copiado para a variável b de maneira independente
+
+a.push(4);
+console.log(a, b); // [ 1, 2, 3, 4 ] [ 1, 2, 3]
+
+// Valores por referencia com objetos
+const a = {
+    nome: 'Jorge',
+    sobrenome: 'Azevedo'
+};
+const b = {...a}; // ... são chamados de spread 
+
+a.nome = 'João';
+console.log(a);
 console.log(b);
+*/
