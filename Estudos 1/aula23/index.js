5376efe4dfc7c5294a2f2ff6875e007fc93f9d6b
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-07-18 09:40:14 -0300

    Update index.js

--- Estudos 1/aula23/index.js
+++ Estudos 1/aula23/index.js
@@ -1,19 +1,34 @@
 /*
-&&
-||
+Avaliação de curto circuito
 
-FALSY
-*false
+&& -> apenas uma expressão falsa para todas serem falsas
+|| -> todas as expressões precisam ser falsas para ser falso 
+
+false && true -> false "retorna o valor da expressão falsa"
+
+console.log('Jorge Azevedo' && 0 && 'Gabrielle Victoria'); // 0
+
+// se todas forem true o JS retorna a ultima expressão
+console.log('Jorge Azevedo' && true && 'Gabrielle Victoria'); // Gabrielle Victoria
+
+// Valores que avaliam em false no JavaScript
+
+false -> valor literal em false
+
+FALSY -> valores que avaliam em false quando necessário
 0
-' "" ``
+'' / "" / ``
 null / undefined
 NaN
 
-Qualquer coisa ao contrario de FALSY é verdadeiro
+*Qualquer coisa ao contrario de valores que avaliam em false no JS é true
 */
 
+
+/*
 function falaOi() {
     return 'Oi';
 }
 let vaiExecultar;
 console.log(vaiExecultar && falaOi());
+*/
