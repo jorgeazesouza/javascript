83fa21ef32da651d4b1439fa52d5145a0e8be769
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-07-20 09:42:31 -0300

    Update index.js

--- Estudos 1/aula23/index.js
+++ Estudos 1/aula23/index.js
@@ -4,6 +4,7 @@ Avaliação de curto circuito
 && -> apenas uma expressão falsa para todas serem falsas
 || -> todas as expressões precisam ser falsas para ser falso 
 
+// && -> AND
 false && true -> false "retorna o valor da expressão falsa"
 
 console.log('Jorge Azevedo' && 0 && 'Gabrielle Victoria'); // 0
@@ -22,13 +23,49 @@ null / undefined
 NaN
 
 *Qualquer coisa ao contrario de valores que avaliam em false no JS é true
-*/
 
-
-/*
 function falaOi() {
     return 'Oi';
 }
-let vaiExecultar;
-console.log(vaiExecultar && falaOi());
+
+// Para verificarmos se vai execultar ou não precisariamos criar uma condicional para mostrar o oi mas ao inves disso vamos criar um shortcut para verificar
+
+// Exemplo 1
+const vaiExecutar = false;
+console.log(vaiExecutar && falaOi()); // false
+
+// Exemplo 2
+let vaiExecutar; 
+console.log(vaiExecutar && falaOi()); // undefined
+
+// Exemplo 3
+const vaiExecutar = 'Joãozinho';
+console.log(vaiExecutar && falaOi()); // Oi
+
+// || -> OR
+// false || true -> false "retorna o valor da expressão verdadeira"
+
+// retorna o primeiro valor verdadeiro
+console.log(0 || false || null || 'Jorge Azevedo' || true); // 'Jorge Azevedo'
+
+// Exemplo 1 - seleção de cores
+const corUsuario = null;
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
+
+// Exemplo 1.1 - seleção de cores
+const corUsuario = 'vermelho';
+const corPadrao = corUsuario || 'preto';
+
+console.log(corPadrao);
+
+// Pegadinha
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e); // false - porem a string escrita false
 */
