5c86cf73bc10dbfabe52031ce9cb53c484020a77
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-07-22 09:54:15 -0300

    Update index.js

--- Estudos 1/aula25/index.js
+++ Estudos 1/aula25/index.js
@@ -1,21 +1,29 @@
-const numero = 10;
 /*
-// Se (numero >= 0 && numero <= 5) ocorrer, faça isso {console.log('O número está entre 0 e 5.')}
-// Se não ocorrer, faça isso {console.log('o número NÃO está entre 0 e 5')}
+// if e else
 
-if(numero >= 0 && numero <= 5) {
-    console.log('O número está entre 0 e 5.');
+const numero = 10;
+
+if (numero >= 0 && numero <= 5) {
+    console.log('O número esta entre 0 e 5.');
 } else {
-    console.log('o número NÃO está entre 0 e 5');
+    console.log('O número NÃO está entre 0 e 5.');
 }
-*/
+// Se (numero >= 0 && numero <= 5) ocorrer, faça isso console.log('O número esta entre 0 e 5.');
+// Se não faça isso console.log('O número NÃO está entre 0 e 5.');
+
+// else if
+const numero = 10;
 
 if (numero >= 0 && numero <= 5) {
     console.log('O número está entre 0 e 5.');
-} else if (numero >= 6 && numero <= 8) {
+} else if (numero <= 8) {
     console.log('O número está entre 6 e 8.');
-} else if (numero >= 9 && numero <= 11) {
+} else if (numero <= 11) {
     console.log('O número está entre 9 e 11.');
 } else {
-    console.log('O número não está entre 0 e 11.');
+    console.log('O número não está entre 0 e 11.')
 }
+// Quando o JavaScript encontra uma condição verdadeira ele execulta a condição e sai do bloco de código mesmo tendo mais de uma condição verdadeira
+
+// podemos criar outros ifs contanto que não estejam ligados um com o outro, serão blocos de códigos separados
+*/
