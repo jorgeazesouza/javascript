9e8ab6f2baa93e3a2ab2459af705f425ece8427b
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2025-09-05 09:09:57 -0300

    Update index.js

--- Estudos 1/aula32/index.js
+++ Estudos 1/aula32/index.js
@@ -1,13 +1,30 @@
-//                    0          1          2
-//                 0  1  2    0  1  2    0  1  2
-const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
-const [lista1, lista2, lista3] = numeros;
-
-console.log(lista3[2]);
-
 /*
+// Exercício anterior
+let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+
 // Atribuição via desestruturação
 // [desestruturação] = [atribuição]
+
+// 1.1
+[a, b, c] = [1, 2, 3]; // reatribuindo valores das variáves através de uma lista
+
+// 1.2
+const numeros = [1, 2, 3];
+[a, b, c] = numeros;
+
+console.log(a, b, c); // 1 2 3
+
+// Respondendo Exercício anterior com a Atribuição via Desestruturação
+let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+
+const letras = [b, c, a];
+[a, b, c] = letras;
+
+console.log(a, b, c); // B C A
 
 // índice        0  1  2  3  4  5  6  7  8
 const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
@@ -16,12 +33,26 @@ const [um, dois, tres, ...resto] = numeros; // ... rest (pegar o resto), ...spre
 console.log(um, dois, tres); // 1000, 2000, 3000
 console.log(resto); // 4000, 5000, 6000, 7000, 8000, 9000
 
+// Lista dentro de outra lista
+// 1.1
 //                    0          1          2
 //                 0  1  2    0  1  2    0  1  2
 const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
 
 console.log(numeros[1][2]); // 6
-const [,[,,seis]] = numeros; // 6
 
+// 1.2
+//                    0          1          2
+//                 0  1  2    0  1  2    0  1  2
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
 const [lista1, lista2, lista3] = numeros;
+
+console.log(lista3[2]); // 9
+
+// Lista dentro de outra lista por Atribuição via Desestruturação
+//                    0          1          2
+//                 0  1  2    0  1  2    0  1  2
+const numeros = [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ];
+const [,[,,seis]] = numeros; // 6
+console.log(seis); // 9
 */
