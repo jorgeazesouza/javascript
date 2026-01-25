4483a3bd0a7186c06931cf1dcc8130b82a25a2bd
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-01-25 09:44:40 -0300

    adiciona aula03 sobre console.log()

---
+++ Curso Udemy/aula03/index.js
@@ -0,0 +1,19 @@
+// String = Texto
+
+console.log('Jorge Azevedo'); // '' - exibir texto
+console.log("Jorge Azevedo"); // "" - exibir texto
+console.log(`Jorge Azevedo`); // `` - exibir texto
+
+// colocar aspas dentro da outra
+console.log("Jorge 'Azevedo' de Souza");
+console.log('Jorge "Azevedo" de Souza');
+
+// Template strings
+console.log(`Jorge 'Azevedo' de "Souza"`);
+
+// Number = Numeros
+console.log(135, 64, 98);
+console.log(15.8, 58.6, 66.8); // Pontos flutuantes
+
+// Mescla
+console.log(15.8, 'Jorge Azevedo');
