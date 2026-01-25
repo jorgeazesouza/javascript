f307390f2af7bb86ad5c29119920b41d29b31987
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-01-25 09:57:41 -0300

    adiciona aula04 sobre comentários em Javascript

---
+++ Curso Udemy/aula04/index.js
@@ -0,0 +1,17 @@
+// Faz um comentário em uma única linha
+
+// Escrever um comentário
+console.log('Olá Mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+
+/* Faz um comentário de tudo dentro do bloco */
+/*
+console.log('Ola Mundo!');
+console.log('Linha 1');
+console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+*/
