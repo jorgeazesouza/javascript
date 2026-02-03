c4e7cced243dc9f3cf7aedef16f5a1f2f4c657e2
Jorge Azevedo <jorgeazesouza2306@gmail.com>
2026-02-03 19:39:45 -0300

    Adicionado aula09 explicando algumas diferenças entre var e let

---
+++ Curso Udemy/aula09/index.js
@@ -0,0 +1,11 @@
+// Diferença entre var e let
+
+// Com var podemos redeclarar uma variável e modificar seu valor
+var nome = 'Jorge';
+var nome = 'Azevedo';
+
+console.log(nome);
+
+/* É criado uma variável com var caso n seja definido mas não é indicado
+nome1 = 'Pablo'; // NÃO FAÇA ISSO
+*/
